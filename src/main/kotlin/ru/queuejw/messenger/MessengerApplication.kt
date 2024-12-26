a89d95aecd6d117c228d162a12eefaca4348314e
pie-with-jam <gahaken3@gmail.com>
2024-12-26 21:19:01 +0300

    Added forced support for HTTPS (TLS) protocol

--- src/main/kotlin/ru/queuejw/messenger/MessengerApplication.kt
+++ src/main/kotlin/ru/queuejw/messenger/MessengerApplication.kt
@@ -3,8 +3,11 @@ package ru.queuejw.messenger
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@RestController
 class MessengerApplication
 
 fun main(args: Array<String>) {
